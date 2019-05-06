891d276a72111248f416a3b89df26d1029876b27
danielmartin0126 <danielmartin0126@gmail.com>
2019-05-06 13:56:42 -0400

    lives functionality

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const test_artist = document.querySelector("#test_artist")
 const form = document.querySelector("#form")
 let currentSong = ""
 let splitSong = []
+let currentUser = {score:0 , lives: 3}
+
 
 fetch('http://localhost:3000/api/v1/lyrics')
     .then(function (response) {
@@ -19,6 +21,12 @@ fetch('http://localhost:3000/api/v1/lyrics')
         currentSong = data[1]
     })
     .then(populateLyrics)
+
+
+function startGame() {
+    currentUser = { score: 0, lives: 3 }
+    resetLives()
+}    
 
 
 function populateLyrics() {
@@ -32,24 +40,55 @@ function populateLyrics() {
 form.addEventListener("submit", function(e) {
     e.preventDefault()
     // debugger
-    // if (e.target.artist.value.toLowerCase() === currentSong.artist.toLowerCase()) {
-    //     console.log("artist correct")
-    // }
-    // else {
-    //     console.log("artist wrong you suck")
-    // }
+    if (e.target.artist.value.toLowerCase() === currentSong.artist.toLowerCase()) {
+        console.log("artist correct")
+        currentUser.score += 50
+    }
+    else {
+        console.log("artist wrong you suck")
+    }
 
-    // if (e.target.title.value.toLowerCase() === currentSong.song_title.toLowerCase()) {
-    //     console.log("song correct")
-    // }
-    // else {
-    //     console.log("song wrong you suck")
-    // }
+    if (e.target.title.value.toLowerCase() === currentSong.song_title.toLowerCase()) {
+        console.log("song correct")
+        currentUser.score += 50
+    }
+    else {
+        console.log("song wrong you suck")
+    }
 
     if (e.target.title.value.toLowerCase() === currentSong.song_title.toLowerCase() || e.target.artist.value.toLowerCase() === currentSong.artist.toLowerCase()) {
         console.log("correct")
+        // getNewSong()
     }
     else {
         console.log("nope")
+        // removeLife()
     }
 })
+
+function removeLife() {
+    let currentHeart = document.querySelector(`#heart${currentUser.lives}`)
+    currentHeart.src = "assets/heart-empty.png"
+    currentUser.lives -= 1
+    if (currentUser.lives === 0) {
+        endGame()
+    } 
+    getNewSong()
+}
+
+function resetLives() {
+    document.querySelector(".incorrect").innerHTML = ""
+    document.querySelector(".incorrect").innerHTML = 
+        `<img class="hearts" id="heart1" src="assets/heart-full.png"></img>
+        <img class="hearts" id="heart2" src="assets/heart-full.png"></img>
+        <img class="hearts" id="heart3" src="assets/heart-full.png"></img>`    
+}
+
+function getNewSong() {
+
+}
+
+function endGame() {
+    // save current user into backend
+    console.log("ya ded")
+}
